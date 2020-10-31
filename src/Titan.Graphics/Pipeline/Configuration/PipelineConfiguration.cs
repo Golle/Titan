3fa6464661f36b185093fd7b880008247510daf9
Jens Andersson <jens.andersson5@gmail.com>
2020-10-31 06:55:44 +0100

    Add render target configuration

--- src/Titan.Graphics/Pipeline/Configuration/PipelineConfiguration.cs
+++ src/Titan.Graphics/Pipeline/Configuration/PipelineConfiguration.cs
@@ -1,20 +1,11 @@
 using Titan.Graphics.D3D11;
 using Titan.Graphics.Shaders;
+using Titan.Windows.Win32.D3D11;
 
 namespace Titan.Graphics.Pipeline.Configuration
 {
-    public class PipelineConfiguration
-    {
-        public ShaderProgramConfiguration[] ShaderPrograms { get; set; }
-    }
-
-    public class ShaderProgramConfiguration
-    {
-        public string Name { get; set; }
-        public VertexShaderDescriptor VertexShader { get; set; }
-        public PixelShaderDescriptor PixelShader { get; set; }
-        public InputLayoutDescriptor[] Layout { get; set; }
-    }
-
-    
+    public record PipelineConfiguration(ShaderProgramConfiguration[] ShaderPrograms, RenderPassConfiguration[] RenderPasses);
+    public record RenderPassConfiguration(string Name, string Renderer, object DepthStencil, RenderTargetConfiguration[] RenderTargets);
+    public record RenderTargetConfiguration(string Name, DXGI_FORMAT Format);
+    public record ShaderProgramConfiguration(string Name, VertexShaderDescriptor VertexShader, PixelShaderDescriptor PixelShader, InputLayoutDescriptor[] Layout);
 }
