4826f4d458c028437bc84dcebf3f63aebc8adcd5
Jens Andersson <jens.andersson5@gmail.com>
2021-01-31 12:21:31 +0100

    DIsable Graphics v1

--- src/Titan/Application.cs
+++ src/Titan/Application.cs
@@ -9,13 +9,7 @@ using Titan.ECS.Assets;
 using Titan.ECS.Systems.Dispatcher;
 using Titan.ECS.World;
 using Titan.EntitySystem;
-using Titan.EntitySystem.Assets;
 using Titan.EntitySystem.Components;
-using Titan.Graphics;
-using Titan.Graphics.Materials;
-using Titan.Graphics.Resources;
-using Titan.Graphics.States;
-using Titan.Graphics.Textures;
 using Titan.Input;
 using Titan.IOC;
 using Titan.Windows;
@@ -28,7 +22,7 @@ namespace Titan
         private readonly ILog _log;
 
         private readonly IWindow _window;
-        private readonly GraphicsSystem _graphicsSystem;
+        //private readonly GraphicsSystem _graphicsSystem;
         private readonly IEventQueue _eventQueue;
         private readonly IMemoryManager _memoryManager;
         private readonly IInputHandler _inputHandler;
@@ -53,10 +47,10 @@ namespace Titan
             return application;
         }
         
-        private Application(IWindow window, GraphicsSystem graphicsSystem, IEventQueue eventQueue, IMemoryManager memoryManager, IInputHandler inputHandler, WorkerPool workerPool, ILog log, IContainer container)
+        private Application(IWindow window, /*GraphicsSystem graphicsSystem,*/ IEventQueue eventQueue, IMemoryManager memoryManager, IInputHandler inputHandler, WorkerPool workerPool, ILog log, IContainer container)
         {
             _window = window;
-            _graphicsSystem = graphicsSystem;
+            //_graphicsSystem = graphicsSystem;
             _eventQueue = eventQueue;
             _memoryManager = memoryManager;
             _inputHandler = inputHandler;
@@ -69,13 +63,13 @@ namespace Titan
         private static WorldBuilder DefaultWorldBuilder() => new WorldBuilder()
             .WithComponent<Transform3D>()
             .WithComponent<Transform2D>()
-            .WithComponent<Asset<Texture>>()
+            //.WithComponent<Asset<Texture>>()
             .WithComponent<Asset<MeshComponent>>()
-            .WithComponent<Texture>()
+            //.WithComponent<Texture>()
             .WithComponent<MeshComponent>()
             .WithSystem<Transform3DSystem>()
-            .WithAssetsManager<Texture2DAssetsManager>()
-            .WithAssetsManager<Model3DAssetsManager>()
+            //.WithAssetsManager<Texture2DAssetsManager>()
+            //.WithAssetsManager<Model3DAssetsManager>()
         ;
             
 
@@ -86,7 +80,7 @@ namespace Titan
             
             (_world, _dispatcher, _managers) = _startup.ConfigureWorld(DefaultWorldBuilder()).Build(_container);
 
-            _startup.OnStart(_world);
+            //_startup.OnStart(_world);
 
             StartMainLoop();
             
@@ -114,7 +108,7 @@ namespace Titan
                 }
                 
                     
-                _graphicsSystem.RenderFrame();
+                //_graphicsSystem.RenderFrame();
                 
                 // Temp code to see FPS
                 frames++;
@@ -148,7 +142,7 @@ namespace Titan
             _window.Initialize((int) configuration.DisplayConfiguration.Width, (int) configuration.DisplayConfiguration.Height, configuration.DisplayConfiguration.Title);
 
             LOGGER.Debug("Initialize the Graphics System");
-            _graphicsSystem.Initialize(configuration.AssetsDirectory.Path, configuration.DisplayConfiguration.RefreshRate, configuration.PipelineConfiguration, true);
+            //_graphicsSystem.Initialize(configuration.AssetsDirectory.Path, configuration.DisplayConfiguration.RefreshRate, configuration.PipelineConfiguration, true);
 
             _startup = (IStartup)_container.CreateInstance(configuration.Startup);
         }
@@ -157,25 +151,25 @@ namespace Titan
         {
             LOGGER.Debug("Initialize memory manager");
             // TODO: not sure how to do this yet. Could have each manager "request" a memory chunk. This should be a part of the graphics system
-            _memoryManager.Initialize(new[]
-            {
-                new ChunkDescriptor("VertexBuffer", (uint) sizeof(VertexBuffer), 2048),
-                new ChunkDescriptor("IndexBuffer", (uint) sizeof(IndexBuffer), 2048),
-                new ChunkDescriptor("ConstantBuffer", (uint) sizeof(ConstantBuffer), 100),
-                new ChunkDescriptor("Materials", (uint) sizeof(Material), 256),
-                new ChunkDescriptor("Shaders", (uint) IntPtr.Size, 1024),
-                new ChunkDescriptor("Texture", (uint) sizeof(Texture2D), 1024),
-                new ChunkDescriptor("ShaderResourceView", (uint) sizeof(ShaderResourceView), 1024),
-                new ChunkDescriptor("RenderTargetView", (uint) sizeof(RenderTargetView), 1024),
-                new ChunkDescriptor("DepthStencilView", (uint) sizeof(DepthStencilView), 10),
-                new ChunkDescriptor("DepthStencilState", (uint) sizeof(DepthStencilState), 10),
-                new ChunkDescriptor("SamplerState", (uint) sizeof(SamplerState), 20),
-            });
+            //_memoryManager.Initialize(new[]
+            //{
+            //    new ChunkDescriptor("VertexBuffer", (uint) sizeof(VertexBuffer), 2048),
+            //    new ChunkDescriptor("IndexBuffer", (uint) sizeof(IndexBuffer), 2048),
+            //    new ChunkDescriptor("ConstantBuffer", (uint) sizeof(ConstantBuffer), 100),
+            //    new ChunkDescriptor("Materials", (uint) sizeof(Material), 256),
+            //    new ChunkDescriptor("Shaders", (uint) IntPtr.Size, 1024),
+            //    new ChunkDescriptor("Texture", (uint) sizeof(Texture2D), 1024),
+            //    new ChunkDescriptor("ShaderResourceView", (uint) sizeof(ShaderResourceView), 1024),
+            //    new ChunkDescriptor("RenderTargetView", (uint) sizeof(RenderTargetView), 1024),
+            //    new ChunkDescriptor("DepthStencilView", (uint) sizeof(DepthStencilView), 10),
+            //    new ChunkDescriptor("DepthStencilState", (uint) sizeof(DepthStencilState), 10),
+            //    new ChunkDescriptor("SamplerState", (uint) sizeof(SamplerState), 20),
+            //});
         }
 
         public void Dispose()
         {
-            _graphicsSystem?.Dispose();
+            //_graphicsSystem?.Dispose();
             _window?.Dispose();
             _container.Dispose();
             _memoryManager.Dispose();
