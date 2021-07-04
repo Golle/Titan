f82dfcac5460818c11ba62dd3ab13883c2e4d078
Jens Andersson <jens.andersson5@gmail.com>
2021-07-04 12:08:40 +0200

    Fixed breakout sample

--- samples/Breakout/Program.cs
+++ samples/Breakout/Program.cs
@@ -2,6 +2,7 @@ using System;
 using Breakout;
 using Titan;
 using Titan.Core.Logging;
+using Titan.Graphics.Windows;
 
 Console.WriteLine($"Hello World!");
 
@@ -21,9 +22,24 @@ namespace Breakout
             collection.Add(new FirstPersonCameraSystem(Window));
         }
 
+        public override EngineConfiguration ConfigureEngine(EngineConfiguration config) =>
+            config with
+            {
+                AssetsPath = "assets"
+            };
+
         public override void OnTerminate()
         {
             Logger.Info("Sandbox application shutting down");
         }
+
+        public override WindowConfiguration ConfigureWindow(WindowConfiguration config) =>
+            config with
+            {
+                Width = 1920,
+                Height = 1080,
+                Title = "Breakout v0.1",
+                Windowed = true
+            };
     }
 }
