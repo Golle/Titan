abe6ffbcf9b2b31f7f52794706d18ebafc513e54
Jens Andersson <jens.andersson5@gmail.com>
2021-08-07 11:03:01 +0200

    Add color to UI shader

--- samples/Titan.Sandbox/assets/shaders/ui_ps.hlsl
+++ samples/Titan.Sandbox/assets/shaders/ui_ps.hlsl
@@ -5,9 +5,9 @@ SamplerState splr : register(s0);
 
 struct PS_INPUT {
     float2 Texture: Texture;
+    float4 Color: Color;
 };
 
-
 float4 main(PS_INPUT input) : SV_TARGET {
-    return tex.Sample(splr, input.Texture);
+    return tex.Sample(splr, input.Texture) * input.Color;
 }
--- samples/Titan.Sandbox/assets/shaders/ui_vs.hlsl
+++ samples/Titan.Sandbox/assets/shaders/ui_vs.hlsl
@@ -5,12 +5,14 @@ cbuffer OrthographicCamera : register(b1)
 
 struct VS_OUTPUT {
     float2 Texture: Texture;
+    float4 Color: Color;
     float4 Position: SV_Position;
 };
 
 struct VS_INPUT {
     float2 Position: POSITION;
     float2 Texture: Texture;
+    float4 Color: Color;
 };    
 
 
@@ -18,5 +20,6 @@ VS_OUTPUT main(VS_INPUT input) {
     VS_OUTPUT output;
     output.Position = mul(float4(input.Position, 1.0f, 1.0f), ViewProjection);
     output.Texture = input.Texture;
+    output.Color = input.Color;
     return output;
 }
