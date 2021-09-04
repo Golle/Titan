60b52cd13606d0d0034ea21abdd256043cd0e3a4
Jens Andersson <jens.andersson5@gmail.com>
2021-09-04 09:18:54 +0200

    Add Easing class

---
+++ src/Titan.Core/Math/Easing.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Titan.Core.Math
+{
+    /// <summary>
+    /// Implementation based on https://easings.net/
+    /// </summary>
+    public static class Easing
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float EaseOutCirc(float x) => MathF.Sqrt(1f - MathF.Pow(x - 1f, 2f));
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float EaseOutSine(float x) => MathF.Sin((x * MathF.PI) / 2);
+    }
+}
