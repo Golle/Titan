99d1473ea4e5e60d4a0e7d5a7f558e581d8f4774
Jens Andersson <jens.andersson5@gmail.com>
2021-09-11 13:50:32 +0200

    Add implementation for ReAllocate

--- src/Titan.Core/Memory/MemoryUtils.cs
+++ src/Titan.Core/Memory/MemoryUtils.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace Titan.Core.Memory
@@ -28,6 +27,25 @@ namespace Titan.Core.Memory
                 OutOfMemoryException();
             }
             return new MemoryChunk(ptr);
+        }
+
+        
+        public static MemoryChunk<T> ReAllocate<T>(ref MemoryChunk<T> memoryChunk, uint count) where T : unmanaged
+        {
+            static void OutOfMemoryException() => throw new OutOfMemoryException("Failed to allocate unmanaged memory.");
+            static void NullPointerException() => throw new InvalidOperationException("Trying to re-allocate a null pointer");
+            if (memoryChunk.AsPointer() == null)
+            {
+                NullPointerException();
+            }
+
+            var size = (uint)(sizeof(T) * count);
+            var newPointer = NativeMemory.Realloc(memoryChunk.AsPointer(), size);
+            if (newPointer == null)
+            {
+                OutOfMemoryException();
+            }
+            return memoryChunk = new MemoryChunk<T>((T*)newPointer, size, count);
         }
 
         public static PinnedMemoryChunk<T> AllocateArray<T>(int size, bool zeroMemory = false) => zeroMemory ? new(GC.AllocateArray<T>(size, false)) :  new (GC.AllocateUninitializedArray<T>(size, false));
