6f84c1423dce18c33ca6bd90c3c96145d6cbfd89
Jens Andersson <jens.andersson5@gmail.com>
2021-12-26 07:27:35 +0100

    Clamp the time spent to 1/60 of a second to avoid crashes when the window is moved or entering fullscreen

--- src/Titan.ECS/Worlds/GameTime.cs
+++ src/Titan.ECS/Worlds/GameTime.cs
@@ -1,43 +1,50 @@
 using System;
 using System.Diagnostics;
+using Titan.Core.Logging;
 
-namespace Titan.ECS.Worlds
+namespace Titan.ECS.Worlds;
+
+internal class GameTime
 {
-    internal class GameTime
+
+    private const float MaxDeltaTime = 1.0f/60f; // 16.7ms
+    private readonly Stopwatch _time;
+    private double _previousFrame;
+    private double _fixedTime;
+    private GameTimeLoop _timeStep;
+    public float FixedTimeStepFrequency { get; }
+    public ref readonly GameTimeLoop Current => ref _timeStep;
+
+    public GameTime(WorldConfiguration config)
     {
-        private readonly Stopwatch _time;
-        private double _previousFrame;
-        private double _fixedTime;
-        private GameTimeLoop _timeStep;
-        public float FixedTimeStepFrequency { get; }
-        public ref readonly GameTimeLoop Current => ref _timeStep;
-
-        public GameTime(WorldConfiguration config)
+        if (config.FixedTimeStep is <= 0f or > 10f)
         {
-            if (config.FixedTimeStep is <= 0f or > 10f)
-            {
-                throw new InvalidOperationException("The fixed timestep can't be less than 0 or greater than 10.");
-            }
-
-            FixedTimeStepFrequency = config.FixedTimeStep;
-            _time = Stopwatch.StartNew();
-            _previousFrame = _time.Elapsed.TotalSeconds;
+            throw new InvalidOperationException("The fixed timestep can't be less than 0 or greater than 10.");
         }
 
-        internal void Update()
-        {
-            var current = _time.Elapsed.TotalSeconds;
-            var delta = current - _previousFrame;
-            _fixedTime += delta;
-            var fixedUpdateCount = 0;
-            while (_fixedTime > FixedTimeStepFrequency)
-            {
-                _fixedTime -= FixedTimeStepFrequency;
-                fixedUpdateCount += 1;
-            }
+        FixedTimeStepFrequency = config.FixedTimeStep;
+        _time = Stopwatch.StartNew();
+        _previousFrame = _time.Elapsed.TotalSeconds;
+    }
 
-            _timeStep = new GameTimeLoop(delta, FixedTimeStepFrequency, fixedUpdateCount);
-            _previousFrame = current;
+    internal void Update()
+    {
+        var current = _time.Elapsed.TotalSeconds;
+        var delta = current - _previousFrame;
+        if (delta > MaxDeltaTime)
+        {
+            Logger.Warning<GameTime>($"Delta time is greater than {MaxDeltaTime}, clamping the value to {MaxDeltaTime}.");
+            delta = MaxDeltaTime;
         }
+        _fixedTime += delta;
+        var fixedUpdateCount = 0;
+        while (_fixedTime > FixedTimeStepFrequency)
+        {
+            _fixedTime -= FixedTimeStepFrequency;
+            fixedUpdateCount += 1;
+        }
+
+        _timeStep = new GameTimeLoop(delta, FixedTimeStepFrequency, fixedUpdateCount);
+        _previousFrame = current;
     }
 }
