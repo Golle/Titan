9eb549b0392c9e88cdca03b17b4015001d409484
Jens Andersson <jens.andersson5@gmail.com>
2022-06-26 06:52:05 +0200

    A merge.

--- src/Titan/Engine.cs
+++ src/Titan/Engine.cs
@@ -1,10 +1,26 @@
 using System;
 using System.Linq;
+using Titan.Assets;
 using Titan.Core.Logging;
+using Titan.Core.Services;
 using Titan.Core.Threading;
 using Titan.ECS;
 using Titan.ECS.TheNew;
+using Titan.Graphics.Images;
+using Titan.Graphics.Loaders;
+using Titan.Graphics.Loaders.Atlas;
+using Titan.Graphics.Loaders.Fonts;
+using Titan.Graphics.Loaders.Materials;
+using Titan.Graphics.Loaders.Models;
+using Titan.Graphics.Loaders.Shaders;
+using Titan.Graphics.Rendering;
+using Titan.Graphics.Rendering.Geometry;
+using Titan.Graphics.Rendering.Sprites;
+using Titan.Graphics.Rendering.Text;
 using Titan.Graphics.Windows;
+using Titan.Sound;
+using Titan.Sound.Loaders;
+using Titan.UI.Debugging;
 
 namespace Titan;
 
@@ -69,56 +85,53 @@ public class Engine
             .Select(config => WorldFactory.Create(config, systemsConfiguration))
             .ToArray();
 
-        
 
-    private void Run()
-    {
-        using var services = new ServiceCollection()
-            .Register(new GameWindow())
-            .Register(new FontManager())
-            .Register(new AtlasManager(100))
-            .Register(new TextManager(200))
-            .Register(new SoundManager());
-            
-        var assetsManager = new AssetsManager()
-            .Register(AssetTypes.Texture, new TextureLoader(new WICImageLoader()))
-            .Register(AssetTypes.Model, new ModelLoader(Resources.Models))
-            .Register(AssetTypes.VertexShader, new VertexShaderLoader())
-            .Register(AssetTypes.PixelShader, new PixelShaderLoader())
-            .Register(AssetTypes.ComputeShader, new ComputeShaderLoader())
-            .Register(AssetTypes.Material, new MaterialsLoader())
-            .Register(AssetTypes.Atlas, new AtlasLoader(services.Get<AtlasManager>()))
-            .Register(AssetTypes.Font, new FontLoader(services.Get<FontManager>()))
-            .Register(AssetTypes.Wave, new WaveLoader(services.Get<SoundManager>()))
-            .Init(new AssetManagerConfiguration(new[]
-            {
-                "manifest.json",
-                "builtin/manifest.json",
-                "builtin/debug_manifest.json"
-            }, MaxConcurrentFileReads: 2));
+        // old
+        //using var services = new ServiceCollection()
+        //    .Register(new FontManager())
+        //    .Register(new AtlasManager(100))
+        //    .Register(new TextManager(200))
+        //    .Register(new SoundManager());
 
-        services
-            .Register(assetsManager)
-            //Rendering (Queues)
-            .Register(new SimpleRenderQueue(1000))
-            .Register(new SpriteRenderQueue(new UIRenderQueueConfiguration(), services.Get<TextManager>(), services.Get<FontManager>()))
-            
-            // NOTE(jens): These should be excluded by some compile flag
-            .Register(new BoundingBoxRenderQueue())
-            .Register(new BoundingBoxRenderer(services.Get<BoundingBoxRenderQueue>()))
-            .Register(new DeferredShadingRenderer())
-            // Note(jens): End note
-            
-            // Renderers
-            .Register(new FullscreenRenderer())
-            .Register(new SpriteRenderer(services.Get<SpriteRenderQueue>()))
-            .Register(new GeometryRenderer(services.Get<SimpleRenderQueue>()))
-            
-            // Sound system
-            .Register(new SoundSystem(services.Get<SoundManager>()))
-            ;
+        //var assetsManager = new AssetsManager()
+        //    .Register(AssetTypes.Texture, new TextureLoader(new WICImageLoader()))
+        //    .Register(AssetTypes.Model, new ModelLoader(Resources.Models))
+        //    .Register(AssetTypes.VertexShader, new VertexShaderLoader())
+        //    .Register(AssetTypes.PixelShader, new PixelShaderLoader())
+        //    .Register(AssetTypes.ComputeShader, new ComputeShaderLoader())
+        //    .Register(AssetTypes.Material, new MaterialsLoader())
+        //    .Register(AssetTypes.Atlas, new AtlasLoader(services.Get<AtlasManager>()))
+        //    .Register(AssetTypes.Font, new FontLoader(services.Get<FontManager>()))
+        //    .Register(AssetTypes.Wave, new WaveLoader(services.Get<SoundManager>()))
+        //    .Init(new AssetManagerConfiguration(new[]
+        //    {
+        //        "manifest.json",
+        //        "builtin/manifest.json",
+        //        "builtin/debug_manifest.json"
+        //    }, MaxConcurrentFileReads: 2));
 
-        var renderingPipeline = _app.ConfigureRenderingPipeline();
+        //services
+        //    .Register(assetsManager)
+        //    //Rendering (Queues)
+        //    .Register(new SimpleRenderQueue(1000))
+        //    .Register(new SpriteRenderQueue(new UIRenderQueueConfiguration(), services.Get<TextManager>(), services.Get<FontManager>()))
+
+        //    // NOTE(jens): These should be excluded by some compile flag
+        //    .Register(new BoundingBoxRenderQueue())
+        //    .Register(new BoundingBoxRenderer(services.Get<BoundingBoxRenderQueue>()))
+        //    .Register(new DeferredShadingRenderer())
+        //    // Note(jens): End note
+
+        //    // Renderers
+        //    .Register(new FullscreenRenderer())
+        //    .Register(new SpriteRenderer(services.Get<SpriteRenderQueue>()))
+        //    .Register(new GeometryRenderer(services.Get<SimpleRenderQueue>()))
+
+        //    // Sound system
+        //    .Register(new SoundSystem(services.Get<SoundManager>()))
+        //    ;
+
+        //var renderingPipeline = _game.ConfigureRenderingPipeline();
 
         using var gameloop = GameLoop.InitAndStart(worlds);
         while (window.Update())
