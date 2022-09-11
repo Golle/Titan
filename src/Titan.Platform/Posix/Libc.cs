e209e8c27b57f1c8ef4cc3a26f0251e70bc3c225
Jens Andersson <jens.andersson5@gmail.com>
2022-09-11 16:22:24 +0200

    Add platform project

---
+++ src/Titan.Platform/Posix/Libc.cs
@@ -0,0 +1,18 @@
+using System.Runtime.InteropServices;
+
+namespace Titan.Platform.Posix;
+
+internal unsafe class Libc
+{
+    //https://sites.uclouvain.be/SystInfo/usr/include/bits/mman.h.html
+    [DllImport("libc", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int getpagesize();
+
+    [DllImport("libc", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void* mmap(void* addr, nuint length, PageProtection prot, PageFlags flags, int fd, ulong offset);
+
+    [DllImport("libc", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int munmap(void* addr, nuint length);
+    [DllImport("libc", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int mprotect(void* addr, nuint len, PageProtection prot);
+}
---
+++ src/Titan.Platform/Posix/PosixPlatformAllocator.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using Titan.Memory;
+
+namespace Titan.Platform.Posix;
+
+internal unsafe struct PosixPlatformAllocator : IPlatformAllocator
+{
+    public static uint PageSize { get; } = (uint)Libc.getpagesize();
+    public static void* Reserve(void* startAddress, uint pages)
+    {
+        var mem = Libc.mmap(startAddress, pages * PageSize, PageProtection.PROT_NONE, PageFlags.MAP_ANON | PageFlags.MAP_SHARED, -1, 0);
+        Debug.Assert(mem != null, "Failed to map memory.");
+        return mem;
+    }
+
+    public static void Commit(void* startAddress, uint pages, uint pageOffset = 0)
+    {
+        var offset = pageOffset * PageSize;
+        var result = Libc.mprotect((byte*)startAddress + offset, pages * PageSize, PageProtection.PROT_READ | PageProtection.PROT_WRITE);
+        Debug.Assert(result == 0);
+    }
+
+    public static void Decommit(void* startAddress, uint pages, uint pageOffset = 0)
+    {
+        var offset = pageOffset * PageSize;
+        var result = Libc.mprotect((byte*)startAddress + offset, pages * PageSize, PageProtection.PROT_NONE);
+        Debug.Assert(result == 0);
+    }
+
+    public static void Release(void* startAddress, uint pages)
+    {
+        var result = Libc.munmap(startAddress, pages * PageSize);
+        Debug.Assert(result == 0);
+    }
+}
